769a3b32be1912f2d5dba36355daa89fa44404f4
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2020-03-09 13:24:11 +0200

    S2E10 HTTP Methods and RESTful APIs

--- server.js
+++ server.js
@@ -7,26 +7,50 @@ const todos = [
 ];
 
 const server = http.createServer((req, res) => {
-  res.writeHead(404, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js'
-  })
+  const { method, url } = req;
+  let body = [];
 
   //console.log(req.headers.authorization);
-  
-  let body = [];
+
   req.on('data', chunk =>{
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
-    console.log(body);
+    
+    
+    let status = 404;
+    const response = {
+      success: false,
+      data: null,
+      error: null,
+    };
+
+    if(method === 'GET' && url === '/todos') {
+      status = 200;
+      response.success = true;
+      response.data = todos;
+    } else if(method === 'POST' && url === '/todos') {
+      const { id, text } = JSON.parse(body);
+      if(!id || !text) {
+        status = 400;
+        response.error = 'Please add id and text';
+      } else {
+        todos.push({ id, text });
+        status = 201;
+        response.success = true;
+        response.data = todos;
+      }
+    }
+
+    res.writeHead(status, {
+      'Content-Type': 'application/json',
+      'X-Powered-By': 'Node.js'
+    })
+
+    res.end(JSON.stringify(response));
   });
 
-  res.end(JSON.stringify({
-    success: false,
-    error: 'Not Found',
-    data: null
-  }));
+  
 });
 
 const PORT = 5000;
